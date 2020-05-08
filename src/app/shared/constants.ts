1c7fc56be5ae2ca05f1a2c294968fecb0af41e79
mBoth <mboth@harriscomputer.com>
2020-05-08 20:36:36 +0300

    live.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -4,7 +4,6 @@ export const httpPostOptions = {
     headers: new HttpHeaders(
         {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
         })
 };
 
